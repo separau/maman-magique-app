964eb5c4105f5608662027682c7dd28acced5f58
separau <747tht@gmail.com>
2025-04-17 11:16:49 +1100

    Delete utils/image_creator.py

--- utils/image_creator.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-def generate_cover_image(name):
-    img = Image.new('RGB', (600, 800), color = (255, 230, 240))
-    d = ImageDraw.Draw(img)
-    d.text((100, 350), f"Maman Magique\n{name}", fill=(120, 0, 60))
-    path = "/mnt/data/cover_image.png"
-    img.save(path)
-    return path
